5a03d8751add0b3809ad9af2ab765ce8efd36750
Vini Tiet <tiet@mira-am.com>
2018-02-27 14:01:08 +0100

    maps front

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -5,6 +5,20 @@ class RequestsController < ApplicationController
     @requests = policy_scope(Request)
     @requests = @requests.where.not(from_lng: nil, from_lat: nil, to_lng: nil, to_lat: nil)
 
+    @markers_from = @requests.map do |request|
+      {
+        lng: request.from_lng,
+        lat: request.from_lat,
+      }
+    end
+
+    @markers_to = @requests.map do |request|
+      {
+        lng: request.to_lng,
+        lat: request.to_lat
+      }
+    end
+
   end
 
   def show
