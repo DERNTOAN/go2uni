108ce25dc96d2f99878ac0083953ac2e21330e87
Henrik Dittmar <henrik.dittmar@gmx.de>
2018-02-27 15:13:51 +0100

    index mobile

--- app/javascript/rides/index.js
+++ app/javascript/rides/index.js
@@ -2,18 +2,19 @@ function inputShowRides(){
   let ride_button = document.getElementById("new-ride-button")
   if (ride_button){
     console.log(ride_button);
+    let form = document.querySelector(".new-ride-form");
     ride_button.addEventListener("click",(event) => {
       event.preventDefault();
       console.log("clicked");
-      let form = document.querySelector(".new-ride-form");
-      form.classList.toggle("hidden")
+      form.classList.toggle("hidden");
+      ride_button.classList.toggle("hidden");
     })
 
-
-
-
-
-
+    let closer = document.querySelector("#x-button")
+    closer.addEventListener("click",(event) => {
+      form.classList.toggle("hidden");
+      ride_button.classList.toggle("hidden");
+    })
   }
 
 
