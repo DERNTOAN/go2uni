80b640254366bffd43fdd5e581bbebeaddbbce18
Anton Castell <castellanton@gmail.com>
2018-02-28 15:03:30 +0100

    added avatar-scss

--- app/controllers/myrequests_controller.rb
+++ app/controllers/myrequests_controller.rb
@@ -1,10 +1,12 @@
 class MyrequestsController < ApplicationController
   def index
     @myrequests = policy_scope(Request).where("user_id = #{params[:id]}")
+    @offers = Offer.all
   end
 
   def show
     @myrequest = Request.find(params[:id])
+    @offers = Offer.where(request_id = @myrequest.id)
     authorize @myrequest
   end
 end
