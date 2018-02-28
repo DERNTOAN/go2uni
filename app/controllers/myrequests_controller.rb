691d458bd364e71256b53babfdf39ad6459369a4
Vini Tiet <tiet@mira-am.com>
2018-03-01 00:51:23 +0100

    distance matrix

--- app/controllers/myrequests_controller.rb
+++ app/controllers/myrequests_controller.rb
@@ -19,9 +19,10 @@ class MyrequestsController < ApplicationController
 
   def show
     @myrequest = Request.find(params[:id])
-    @offers = Offer.where(request: @myrequest)
+    @offers = Offer.where(request: @myrequest).where(confirmed: nil)
     @rides = @offers.map { |offer| offer.ride }
     authorize @myrequest
+
 
     @marker_from_passenger = {
       lng: @myrequest.from_lng,
@@ -29,32 +30,64 @@ class MyrequestsController < ApplicationController
       avatar: @myrequest.user.photo.url
     }
 
+    @walking_distances = @rides.map do |ride|
+      url_walk_to_car= "https://maps.googleapis.com/maps/api/distancematrix/json?origins=#{@myrequest.from_lat},#{@myrequest.from_lng}&destinations=#{ride.from_lat},#{ride.from_lng}&mode=walking&key=#{ENV['GOOGLE_API_SERVER_KEY']}"
+      distances_walk_to_car = JSON.parse(open(url_walk_to_car).read)
 
-    @marker_to = {
-      lng: @myrequest.to_lng,
-      lat: @myrequest.to_lat
+      url_ride= "https://maps.googleapis.com/maps/api/distancematrix/json?origins=#{ride.from_lat},#{ride.from_lng}&destinations=#{ride.to_lat},#{ride.to_lng}&mode=driving&key=#{ENV['GOOGLE_API_SERVER_KEY']}"
+      distances_ride = JSON.parse(open(url_ride).read)
+
+
+      url_walk_to_dest= "https://maps.googleapis.com/maps/api/distancematrix/json?origins=#{ride.to_lat},#{ride.to_lng}&destinations=#{@myrequest.to_lat},#{@myrequest.to_lng}&mode=walking&key=#{ENV['GOOGLE_API_SERVER_KEY']}"
+      distances_walk_to_dest = JSON.parse(open(url_walk_to_dest).read)
+
+      unless([distances_walk_to_car["rows"][0]["elements"][0]["status"],
+        distances_ride["rows"][0]["elements"][0]["status"],
+        distances_walk_to_dest["rows"][0]["elements"][0]["status"]].include? "ZERO_RESULTS")
+      {ride: ride,
+      distance_matrix: {
+        walk_to_car_duration_text: distances_walk_to_car["rows"][0]["elements"][0]["duration"]["text"],
+        walk_to_car_duration_s: distances_walk_to_car["rows"][0]["elements"][0]["duration"]["value"],
+        walk_to_car_distance_m: distances_walk_to_car["rows"][0]["elements"][0]["distance"]["value"],
+        ride_duration_text: distances_ride["rows"][0]["elements"][0]["duration"]["text"],
+        ride_duration_s: distances_ride["rows"][0]["elements"][0]["duration"]["value"],
+        ride_distance_m: distances_ride["rows"][0]["elements"][0]["distance"]["value"],
+        walk_to_dest_duration_text: distances_walk_to_dest["rows"][0]["elements"][0]["duration"]["text"],
+        walk_to_dest_duration_s: distances_walk_to_dest["rows"][0]["elements"][0]["duration"]["value"],
+        walk_to_dest_distance_m: distances_walk_to_dest["rows"][0]["elements"][0]["distance"]["value"],
+      }
     }
+    end
 
-    @drivers = @rides.map do |ride|
-      {
 
-        from: {
-          lng: ride.from_lng,
-          lat: ride.from_lat,
-          },
+  end
+
+
+  @marker_to = {
+    lng: @myrequest.to_lng,
+    lat: @myrequest.to_lat
+  }
+
+  @drivers = @rides.map do |ride|
+    {
+
+      from: {
+        lng: ride.from_lng,
+        lat: ride.from_lat,
+        },
 
         to: {
-            lng: ride.to_lng,
-            lat: ride.to_lat,
+          lng: ride.to_lng,
+          lat: ride.to_lat,
           },
 
 
-            avatar: ride.user.photo.url,
-            id: ride.user.id,
-            first_name: ride.user.first_name,
-            last_name: ride.user.last_name
-          }
-        end
-
+          avatar: ride.user.photo.url,
+          id: ride.user.id,
+          first_name: ride.user.first_name,
+          last_name: ride.user.last_name
+        }
       end
+
+    end
   end
--- app/javascript/packs/mapshowmyrequest.js
+++ app/javascript/packs/mapshowmyrequest.js
@@ -33,10 +33,6 @@ const from_self      = new google.maps.LatLng(from);
 const to_self      = new google.maps.LatLng(to);
 const destination = new google.maps.LatLng(to);
 
-const mapOptions = {
-  zoom: 8,
-  center: from_self
-}
 
 const self_icon = {
   url: from.avatar,
@@ -46,21 +42,313 @@ const self_icon = {
   }
 }
 
+var styledMapType = new google.maps.StyledMapType(
+
+  [
+  {
+    "elementType": "geometry",
+    "stylers": [
+    {
+      "color": "#ebe3cd"
+    }
+    ]
+  },
+  {
+    "elementType": "labels.text.fill",
+    "stylers": [
+    {
+      "color": "#523735"
+    }
+    ]
+  },
+  {
+    "elementType": "labels.text.stroke",
+    "stylers": [
+    {
+      "color": "#f5f1e6"
+    }
+    ]
+  },
+  {
+    "featureType": "administrative",
+    "elementType": "geometry.stroke",
+    "stylers": [
+    {
+      "color": "#c9b2a6"
+    }
+    ]
+  },
+  {
+    "featureType": "administrative.land_parcel",
+    "stylers": [
+    {
+      "visibility": "off"
+    }
+    ]
+  },
+  {
+    "featureType": "administrative.land_parcel",
+    "elementType": "geometry.stroke",
+    "stylers": [
+    {
+      "color": "#dcd2be"
+    }
+    ]
+  },
+  {
+    "featureType": "administrative.land_parcel",
+    "elementType": "labels.text.fill",
+    "stylers": [
+    {
+      "color": "#ae9e90"
+    }
+    ]
+  },
+  {
+    "featureType": "administrative.neighborhood",
+    "stylers": [
+    {
+      "visibility": "off"
+    }
+    ]
+  },
+  {
+    "featureType": "landscape.natural",
+    "elementType": "geometry",
+    "stylers": [
+    {
+      "color": "#dfd2ae"
+    }
+    ]
+  },
+  {
+    "featureType": "poi",
+    "elementType": "geometry",
+    "stylers": [
+    {
+      "color": "#dfd2ae"
+    }
+    ]
+  },
+  {
+    "featureType": "poi",
+    "elementType": "labels.text",
+    "stylers": [
+    {
+      "visibility": "off"
+    }
+    ]
+  },
+  {
+    "featureType": "poi",
+    "elementType": "labels.text.fill",
+    "stylers": [
+    {
+      "color": "#93817c"
+    }
+    ]
+  },
+  {
+    "featureType": "poi.business",
+    "stylers": [
+    {
+      "visibility": "off"
+    }
+    ]
+  },
+  {
+    "featureType": "poi.park",
+    "elementType": "geometry.fill",
+    "stylers": [
+    {
+      "color": "#a5b076"
+    }
+    ]
+  },
+  {
+    "featureType": "poi.park",
+    "elementType": "labels.text",
+    "stylers": [
+    {
+      "visibility": "off"
+    }
+    ]
+  },
+  {
+    "featureType": "poi.park",
+    "elementType": "labels.text.fill",
+    "stylers": [
+    {
+      "color": "#447530"
+    }
+    ]
+  },
+  {
+    "featureType": "road",
+    "elementType": "geometry",
+    "stylers": [
+    {
+      "color": "#f5f1e6"
+    }
+    ]
+  },
+  {
+    "featureType": "road",
+    "elementType": "labels",
+    "stylers": [
+    {
+      "visibility": "off"
+    }
+    ]
+  },
+  {
+    "featureType": "road.arterial",
+    "elementType": "geometry",
+    "stylers": [
+    {
+      "color": "#fdfcf8"
+    }
+    ]
+  },
+  {
+    "featureType": "road.highway",
+    "elementType": "geometry",
+    "stylers": [
+    {
+      "color": "#f8c967"
+    }
+    ]
+  },
+  {
+    "featureType": "road.highway",
+    "elementType": "geometry.stroke",
+    "stylers": [
+    {
+      "color": "#e9bc62"
+    }
+    ]
+  },
+  {
+    "featureType": "road.highway.controlled_access",
+    "elementType": "geometry",
+    "stylers": [
+    {
+      "color": "#e98d58"
+    }
+    ]
+  },
+  {
+    "featureType": "road.highway.controlled_access",
+    "elementType": "geometry.stroke",
+    "stylers": [
+    {
+      "color": "#db8555"
+    }
+    ]
+  },
+  {
+    "featureType": "road.local",
+    "elementType": "labels.text.fill",
+    "stylers": [
+    {
+      "color": "#806b63"
+    }
+    ]
+  },
+  {
+    "featureType": "transit.line",
+    "elementType": "geometry",
+    "stylers": [
+    {
+      "color": "#dfd2ae"
+    }
+    ]
+  },
+  {
+    "featureType": "transit.line",
+    "elementType": "labels.text.fill",
+    "stylers": [
+    {
+      "color": "#8f7d77"
+    }
+    ]
+  },
+  {
+    "featureType": "transit.line",
+    "elementType": "labels.text.stroke",
+    "stylers": [
+    {
+      "color": "#ebe3cd"
+    }
+    ]
+  },
+  {
+    "featureType": "transit.station",
+    "elementType": "geometry",
+    "stylers": [
+    {
+      "color": "#dfd2ae"
+    }
+    ]
+  },
+  {
+    "featureType": "water",
+    "elementType": "geometry.fill",
+    "stylers": [
+    {
+      "color": "#b9d3c2"
+    }
+    ]
+  },
+  {
+    "featureType": "water",
+    "elementType": "labels.text",
+    "stylers": [
+    {
+      "visibility": "off"
+    }
+    ]
+  },
+  {
+    "featureType": "water",
+    "elementType": "labels.text.fill",
+    "stylers": [
+    {
+      "color": "#92998d"
+    }
+    ]
+  }
+  ],
+  {name: 'Styled Map'});
+
+
+const mapOptions = {
+  zoom: 8,
+  center: from_self,
+  mapTypeControlOptions: {
+    mapTypeIds: ['roadmap', 'satellite', 'hybrid', 'terrain',
+    'styled_map']
+  }
+}
+
 const map = new google.maps.Map(mapElement, mapOptions);
 
+//Associate the styled map with the MapTypeId and set it to display.
+map.mapTypes.set('styled_map', styledMapType);
+map.setMapTypeId('styled_map');
 const self_marker = new google.maps.Marker(
-  {
+{
   position: from_self,
   map: map,
   icon: self_icon
-  });
+});
 
 const self_to_marker = new google.maps.Marker(
-  {
+{
   position: to_self,
   map: map,
   icon: self_icon
-  });
+});
 
 const travel_time = {};
 
@@ -78,7 +366,7 @@ counterparts.forEach( (counterpart) => {
         width: 30,
         height: 30
       }
-  }});
+    }});
 
   const request_walk_to_car = {
     origin:      from_self,
@@ -107,18 +395,10 @@ counterparts.forEach( (counterpart) => {
     }
   });
 
-  directionsService.route(request_drive, function(response, status) {
-        console.log(status);
 
-    const time_drive = showRoute(response, status, map, "#0000ff");
-    if(time_drive) {
-
-      travel_time[counterpart.first_name]['time_drive'] = time_drive.reduce((a, b) => a + b, 0);
-    }
-  });
 
   directionsService.route(request_walk_to_dest, function(response, status) {
-        console.log(status);
+    console.log(status);
 
     const time_walk_to_dest = showRoute(response, status, map, "#00ff00")
     if(time_walk_to_dest) {
@@ -128,10 +408,24 @@ counterparts.forEach( (counterpart) => {
 
   });
 
+  directionsService.route(request_drive, function(response, status) {
+    console.log(status);
+
+    const time_drive = showRoute(response, status, map, "#0000ff");
+    if(time_drive) {
+
+      travel_time[counterpart.first_name]['time_drive'] = time_drive.reduce((a, b) => a + b, 0);
+    }
+  });
+
+
+
 });
 
-console.log(travel_time);
+map.setOptions( { zoom: 1 } )
 
 
 
 }
+
+
