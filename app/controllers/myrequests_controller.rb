e577b42e99711fbdf197519cec23d23719b2937a
Vini Tiet <tiet@mira-am.com>
2018-02-28 18:10:04 +0100

    travel time calculation WIP

--- app/controllers/myrequests_controller.rb
+++ app/controllers/myrequests_controller.rb
@@ -19,18 +19,42 @@ class MyrequestsController < ApplicationController
 
   def show
     @myrequest = Request.find(params[:id])
-    @offers = Offer.all
+    @offers = Offer.where(request: @myrequest)
+    @rides = @offers.map { |offer| offer.ride }
     authorize @myrequest
-    @markers_from =
-      {
-        lng: @myrequest.from_lng,
-        lat: @myrequest.from_lat,
-      }
 
-    @markers_to =
+    @marker_from_passenger = {
+      lng: @myrequest.from_lng,
+      lat: @myrequest.from_lat,
+      avatar: @myrequest.user.photo.url
+    }
+
+
+    @marker_to = {
+      lng: @myrequest.to_lng,
+      lat: @myrequest.to_lat
+    }
+
+    @drivers = @rides.map do |ride|
       {
-        lng: @myrequest.to_lng,
-        lat: @myrequest.to_lat
-      }
+
+        from: {
+          lng: ride.from_lng,
+          lat: ride.from_lat,
+          },
+
+        to: {
+            lng: ride.to_lng,
+            lat: ride.to_lat,
+          },
+
+
+            avatar: ride.user.photo.url,
+            id: ride.user.id,
+            first_name: ride.user.first_name,
+            last_name: ride.user.last_name
+          }
+        end
+
+      end
   end
-end
---
+++ app/javascript/packs/mapshowmyrequest.js
@@ -0,0 +1,173 @@
+
+const mapElement = document.getElementById('mapshowmyrequest');
+if (mapElement) { // don't try to build a map if there's no div#map to inject in
+  const from = JSON.parse(mapElement.dataset.marker_from_self);
+const to = JSON.parse(mapElement.dataset.marker_to);
+const counterparts = JSON.parse(mapElement.dataset.counterparts);
+const directionsService = new google.maps.DirectionsService();
+
+const from_self      = new google.maps.LatLng(from);
+const to_self      = new google.maps.LatLng(to);
+const destination = new google.maps.LatLng(to);
+
+const mapOptions = {
+  zoom: 8,
+  center: from_self
+}
+
+const self_icon = {
+  url: from.avatar,
+  scaledSize: {
+    width: 70,
+    height: 70
+  }
+}
+
+const map = new google.maps.Map(mapElement, mapOptions);
+
+
+
+const self_marker = new google.maps.Marker(
+{
+  position: from_self,
+  map: map,
+  icon: self_icon
+}
+
+);
+
+const self_to_marker = new google.maps.Marker(
+{
+  position: to_self,
+  map: map,
+  icon: self_icon
+}
+
+);
+
+const travel_time = {};
+
+
+counterparts.forEach( (counterpart) => {
+
+  const from_counterpart   = new google.maps.LatLng(counterpart.from);
+  const to_counterpart   = new google.maps.LatLng(counterpart.to);
+
+  const counterpart_from_marker = new google.maps.Marker({
+    position: from_counterpart,
+    map: map,
+    icon: {
+      url: counterpart.avatar,
+      scaledSize: {
+        width: 30,
+        height: 30
+      }
+    }
+
+  });
+
+  const request_walk = {
+    origin:      from_self,
+    destination: from_counterpart,
+    travelMode:  google.maps.TravelMode.WALKING
+  };
+
+  const request_walk_to_dest = {
+    origin:      to_counterpart,
+    destination: to_self,
+    travelMode:  google.maps.TravelMode.WALKING
+  };
+
+
+
+  const request_drive = {
+    origin:      from_counterpart,
+    destination: to_counterpart,
+    travelMode:  google.maps.TravelMode.DRIVING
+  };
+
+  directionsService.route(request_walk, function(response, status) {
+    if (status == google.maps.DirectionsStatus.OK) {
+console.log(counterpart.first_name)
+console.log(response.routes[0].legs.map(leg => leg.duration.value))
+
+
+
+
+
+
+
+
+  const directionsDisplay = new google.maps.DirectionsRenderer();
+  directionsDisplay.setMap(map);
+
+
+      const DirectionsRendererOptions = {
+        polylineOptions: {
+          strokeColor: "#ff0000",
+          strokeOpacity: 0.5,
+          strokeWeight: 4
+        },
+        suppressMarkers: true
+      };
+      directionsDisplay.setOptions(DirectionsRendererOptions);
+      directionsDisplay.setDirections(response);
+    }
+  });
+
+  directionsService.route(request_drive, function(response, status) {
+    if (status == google.maps.DirectionsStatus.OK) {
+
+console.log(counterpart.first_name)
+console.log(response.routes[0].legs.map(leg => leg.duration.value))
+
+
+
+  const directionsDisplay = new google.maps.DirectionsRenderer();
+  directionsDisplay.setMap(map);
+
+      const DirectionsRendererOptions = {
+        polylineOptions: {
+          strokeColor: "#0000ff",
+          strokeOpacity: 0.5,
+          strokeWeight: 4
+        },
+        suppressMarkers: true
+      };
+      directionsDisplay.setOptions(DirectionsRendererOptions);
+      directionsDisplay.setDirections(response);
+    }
+  });
+
+  directionsService.route(request_walk_to_dest, function(response, status) {
+    if (status == google.maps.DirectionsStatus.OK) {
+
+console.log(counterpart.first_name)
+console.log(response.routes[0].legs.map(leg => leg.duration.value))
+
+
+  const directionsDisplay = new google.maps.DirectionsRenderer();
+  directionsDisplay.setMap(map);
+
+      const DirectionsRendererOptions = {
+        polylineOptions: {
+          strokeColor: "#00ff00",
+          strokeOpacity: 0.5,
+          strokeWeight: 4
+        },
+        suppressMarkers: true
+      };
+      directionsDisplay.setOptions(DirectionsRendererOptions);
+      directionsDisplay.setDirections(response);
+    }
+  });
+
+
+
+
+
+});
+
+
+
+}
