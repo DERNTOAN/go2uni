6baccdfb7b8fa45190055ac44843962cc6983a32
Vini Tiet <tiet@mira-am.com>
2018-03-02 12:28:50 +0100

    zoom correction

--- app/javascript/packs/map.js
+++ app/javascript/packs/map.js
@@ -6,8 +6,12 @@ if (mapElement) { // don't try to build a map if there's no div#map to inject in
 const counterparts = JSON.parse(mapElement.dataset.counterparts);
 const map_origin      = new google.maps.LatLng(counterparts[0].from);
 const car_icon = { scaledSize: { width: 30, height: 30 }, url: "https://res.cloudinary.com/dekx98imz/image/upload/v1519916998/noun_128968_cc.png" }
-const map = setupMap(mapElement, map_origin, 15);
-
+const mapbounds = JSON.parse(mapElement.dataset.mapbounds);
+const minbound      = new google.maps.LatLng(mapbounds.min);
+const maxbound      = new google.maps.LatLng(mapbounds.max);
+const bounds = new google.maps.LatLngBounds(minbound, maxbound);
+const map = setupMap(mapElement, map_origin, 13);
+map.fitBounds(bounds);
 counterparts.forEach((counterpart) => {
 
     const from_counterpart   = new google.maps.LatLng(counterpart.from);
