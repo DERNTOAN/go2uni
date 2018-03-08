4b1d3d0ccbaa5a6ba3ba931100e79dcb89cd1996
Anton Castell <castellanton@gmail.com>
2018-03-08 18:44:45 +0100

    maps should work now

--- app/controllers/rides_controller.rb
+++ app/controllers/rides_controller.rb
@@ -141,7 +141,7 @@ class RidesController < ApplicationController
 
   def update_session
     if params[:lat] != session[:location]["lat"] && params[:lat] != nil
-      session[:location] = { lat: params[:lat].to_f, lng: params[:lng].to_f }
+      session[:location] = { "lat"=> params[:lat].to_f, "lng"=> params[:lng].to_f }
     end
   end
 end
