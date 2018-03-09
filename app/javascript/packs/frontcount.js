0021da30a24dabe471214dfde699de7b9826da6b
Henrik Dittmar <henrik.dittmar@gmx.de>
2018-03-09 12:40:37 +0100

    final

--- app/javascript/packs/frontcount.js
+++ app/javascript/packs/frontcount.js
@@ -1,19 +1,19 @@
 import CountUp from "countup.js"
-const numAnim = new CountUp("bayreuth-counter", 15, 45, 0, 60,{useEasing: false});
+const numAnim = new CountUp("bayreuth-counter", 15, 61, 0, 120,{useEasing: false});
 if (!numAnim.error) {
     numAnim.start();
 } else {
     console.error(numAnim.error);
 }
 
-const numAnim3 = new CountUp("aachen-counter", 11, 30, 0, 50,{useEasing: false});
+const numAnim3 = new CountUp("aachen-counter", 11, 39, 0, 140,{useEasing: false});
 if (!numAnim3.error) {
     numAnim3.start();
 } else {
     console.error(numAnim3.error);
 }
 
-const numAnim2 = new CountUp("braunschweig-counter", 10, 25, 0,40,{useEasing: false});
+const numAnim2 = new CountUp("braunschweig-counter", 10, 33, 0,130,{useEasing: false});
 if (!numAnim2.error) {
     numAnim2.start();
 } else {
